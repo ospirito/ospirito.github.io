e856a2637ea91c26afe1592586baaa6b748bf517
Oliver Spirito <ospirito@onetrust.com>
2021-03-16 15:34:18 -0400

    question edits, add HTML table

--- ot/MOBTicket.js
+++ ot/MOBTicket.js
@@ -13,10 +13,49 @@ function showHideQuestions() {
         break;
     }
   }
+
+  function generateTables(){
+    generateMarkdown()
+    generateHTMLTable()
+  }
   
   
   function generateMarkdown() {
     var output = ["||Question||Answer||"]
+    parseQuestions().forEach(e=>{
+      output.push(`|${e.q}|${e.a}|`)
+    })
+    document.querySelector("#output").value = output.join("\n")
+  }
+
+  function generateHTMLTable(){
+    var table = document.createElement("table")
+    var headers = ["Question", "Answer"]
+    var header = document.createElement("tr")
+    headers.forEach(headerText=>{
+      var head = document.createElement("th")
+      head.innerText = headerText
+      header.appendChild(head)
+    })
+    table.appendChild(header)
+
+    parseQuestions().forEach(e=>{
+      var row = document.createElement("tr")
+      var question = document.createElement("td")
+      question.innerText = e.q
+
+      var answer = document.createElement("td")
+      answer.innerText = e.a
+
+      row.append(question)
+      row.append(answer)
+      table.append(row)
+    })
+    document.querySelector("#tableOutput").replaceWith(table)
+  }
+
+  function parseQuestions(){
+    var output = []
     document.querySelectorAll(".question").forEach(e => {
       try {
         var q = e.querySelector("label").innerHTML
@@ -29,14 +68,10 @@ function showHideQuestions() {
             return
           }
         }
-  
-        output.push("|" + q + "|" + a + "|")
+        output.push({q:q, a:a})
       } catch (j) {
-  
+        
       }
-  
     })
-  
-    document.querySelector("#output").value = output.join("\n")
+    return output
   }
-  
